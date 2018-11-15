3cd101036331251ad9921e68dbad5fc3451a52ea
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-15 11:56:07 +0530

    created Actor model

---
+++ IMDB.Domain/Entities/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMDB.Domain.Entities
+{
+    public class Actor  //as the domain model is in different VS project, it mut be marked as public
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public char Sex { get; set; }
+        public DateTime Data { get; set; }
+        public string Bio { get; set; }
+    }
+}
