7a2a991760a07f414b7a10b82ac3c87a082e777e
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-15 11:59:29 +0530

    used a repository pattern for retrieving and saving logic of models data, so made an interafce -actorsrepository

---
+++ IMDB.Domain/Abstract/IActorsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMDB.Domain.Entities;
+
+namespace IMDB.Domain.Abstract
+{
+    public interface IActorsRepository
+    {
+        IEnumerable<Actor> Actors { get; }
+    }
+}
