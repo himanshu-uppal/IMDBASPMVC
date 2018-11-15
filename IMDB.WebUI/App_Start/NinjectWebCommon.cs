5890604851c5fb8da4cce1a5470db57cb186985f
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-15 11:45:02 +0530

    Added NinjectDependencyResolver for any dependencies in the project

--- IMDB.WebUI/App_Start/NinjectWebCommon.cs
+++ IMDB.WebUI/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,8 @@ namespace IMDB.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new
+            IMDB.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
