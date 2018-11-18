90eae36e1eca5ef58324c1595fb1da9cc1a0247c
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-18 17:59:16 +0530

    made form for shipping details using display name and property forech implementaion

--- IMDB.WebUI/Controllers/CartController.cs
+++ IMDB.WebUI/Controllers/CartController.cs
@@ -62,5 +62,10 @@ namespace IMDB.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
